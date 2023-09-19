0638b08f2bb249f8d99d65e5fa50ce658fcbd5ac
Jordi <jordi8101@gmail.com>
2023-09-18 22:48:12 -0700

    fix: updated imports

--- webserver.py
+++ webserver.py
@@ -11,26 +11,37 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.url = urlparse(self.path)
 
-        # Create redis connection
-        r = redis.StrictRedis(host="localhost", port=6379, db=0)
+        ends_with_html = re.search(r"\.html$", self.url.path)
 
-        # Get data from redis
-        book = r.get(self.url.path[1:])
+        if ends_with_html:
+            # Create redis connection
+            r = redis.StrictRedis(host="localhost", port=6379, db=0)
 
-        # Close redis connection
-        r.connection_pool.disconnect()
+            # Get data from redis
+            url_path = self.url.path.split("/")[2].lower()
+            book = r.get(url_path)
 
-        # Set response
-        self.send_response(200)
-        # Write headers
-        self.wfile.write(self.get_response(book).encode("utf-8"))
+            # Close redis connection
+            r.connection_pool.disconnect()
+
+            # Set response
+            self.send_response(200)
+            # Write headers
+            self.wfile.write(self.get_response(book).encode("utf-8"))
+
+    def validate_endpoint(self):
+        endpoint = self.url.path.split("/")[1].lower()
+
+        return endpoint == "" or endpoint == "index.html"
 
     def get_response(self, book):
+        if self.validate_endpoint():
+            with open("html/index.html") as f:
+                return f.read()
         if book:
             return book.decode("utf-8")
         return """
-            <h1> Hola Web </h1>
-            <p> Hello World </p>
+            File not found
         """
 
 
@@ -39,22 +50,26 @@ def set_redis_data():
     directory = "html/books"
 
     for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+
         if r.exists(file_name):
-            print(f"File {file_name} already loaded")
-            continue
+            book = r.get(file_name)
+
+            if book.decode("utf-8") != open(file_path, "r", encoding="utf-8").read():
+                with open(file_path, "r", encoding="utf-8") as file:
+                    r.set(file_name, file.read())
+                    print(f"File {file_name} updated")
+                continue
+            else:
+                print(f"File {file_name} already loaded")
+                continue
         else:
-            file_path = os.path.join(directory, file_name)
             with open(file_path, "r", encoding="utf-8") as file:
                 r.set(file_name, file.read())
                 print(f"File {file_name} loaded")
 
     r.connection_pool.disconnect()
 
-
-books = {
-    "1": "book 1",
-    "2": "book 2",
-}
 
 if __name__ == "__main__":
     print("Server starting...")
